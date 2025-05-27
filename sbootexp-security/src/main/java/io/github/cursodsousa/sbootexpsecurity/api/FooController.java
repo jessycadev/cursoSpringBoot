497b0bd82d96da7067773b13d0a382704f3d229a
jessycadev <developerjessicaborges@gmail.com>
2025-05-27 19:04:11 -0300

    codigo do curso de spring boot

---
+++ sbootexp-security/src/main/java/io/github/cursodsousa/sbootexpsecurity/api/FooController.java
@@ -0,0 +1,28 @@
+package io.github.cursodsousa.sbootexpsecurity.api;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FooController {
+
+    @GetMapping("/public")
+    public ResponseEntity<String> publicRoute(){
+        return ResponseEntity.ok("Public route ok!");
+    }
+
+    @GetMapping("/private")
+    public ResponseEntity<String> privateRoute(Authentication authentication){
+        System.out.println(authentication.getClass());
+        return ResponseEntity.ok("Private route ok! Usuário conectado: " + authentication.getName());
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<String> adminRoute(){
+        return ResponseEntity.ok("Admin route ok!");
+    }
+}
