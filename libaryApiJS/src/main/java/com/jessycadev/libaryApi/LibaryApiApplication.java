4d98d0fd378369a068b60e57bc3cda84a7837e4d
jessycadev <developerjessicaborges@gmail.com>
2025-05-28 17:18:07 -0300

    aula sobre datasource

---
+++ libaryApiJS/src/main/java/com/jessycadev/libaryApi/LibaryApiApplication.java
@@ -0,0 +1,13 @@
+package com.jessycadev.libaryApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibaryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibaryApiApplication.class, args);
+	}
+
+}
---
+++ libaryApiJS/src/test/java/com/jessycadev/libaryApi/LibaryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jessycadev.libaryApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibaryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
